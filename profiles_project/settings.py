db240a20379eb6e8116cee2de0495c0f955c76f3
root <root@ip-172-31-11-255.ap-south-1.compute.internal>
2023-07-02 09:54:09 +0000

    Modify host

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'knv_*-y7$w%i&vvth4(le^x9#&jti7@io(1w+l99kewe+7l*!)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
